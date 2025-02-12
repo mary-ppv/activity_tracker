c817c820f6ceb55d3a4b598613e985dc81aadf16
Мария Попова <mariapopova@MacBook-Air-Maria.local>
2025-02-12 23:42:43 +0300

    first commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,23 +1,53 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/go1fl-4-sprint-final/internal/spentcalories"
 )
 
 var (
-	StepLength = 0.65 // длина шага в метрах
+	StepLength = 0.65
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// ваш код ниже
+	slice := strings.Split(data, ",")
+	if len(slice) != 2 {
+		return 0, 0, errors.New("ошибка")
+	}
+
+	steps, err := strconv.Atoi(slice[0])
+	if err != nil {
+		return 0, 0, errors.New("ошибка")
+	}
+
+	duration, err := time.ParseDuration(slice[1])
+	if err != nil {
+		return 0, 0, errors.New("ошибка")
+	}
+	return steps, duration, nil
 }
 
-// DayActionInfo обрабатывает входящий пакет, который передаётся в
-// виде строки в параметре data. Параметр storage содержит пакеты за текущий день.
-// Если время пакета относится к новым суткам, storage предварительно
-// очищается.
-// Если пакет валидный, он добавляется в слайс storage, который возвращает
-// функция. Если пакет невалидный, storage возвращается без изменений.
 func DayActionInfo(data string, weight, height float64) string {
-	// ваш код ниже
+	steps, duration, err := parsePackage(data)
+
+	if err != nil {
+		fmt.Errorf("ошибка в ходе выполнения программы: %v", err)
+		return ""
+	}
+
+	if steps <= 0 {
+		return ""
+	}
+
+	distanceKm := (StepLength * float64(steps)) / 1000
+	kalories := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
+
+	final := fmt.Sprintf("Количество шагов: %d./nДистанция составила %.2f км./nВы сожгли %.2f ккал.", steps, distanceKm, kalories)
+
+	return final
 }
